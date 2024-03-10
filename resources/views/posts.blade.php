a8cb7884524d85a6fd19ed6c2ad16c05e27de6db
dmawardi <dmawardi@gmail.com>
2024-03-10 15:31:30 +0800

    Completed blog routes with caching

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem aspernatur provident quasi quo eveniet hic placeat inventore praesentium architecto, esse, a quas animi aut fugiat perspiciatis excepturi nihil rerum illum.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem aspernatur provident quasi quo eveniet hic placeat inventore praesentium architecto, esse, a quas animi aut fugiat perspiciatis excepturi nihil rerum illum.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem aspernatur provident quasi quo eveniet hic placeat inventore praesentium architecto, esse, a quas animi aut fugiat perspiciatis excepturi nihil rerum illum.</p>
+    </article>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{slug}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if(! file_exists($path)) {
+        // dd is used to pass messages for debugging
+        // dd('File does not exist.');
+
+        // abort is used to return a status code and a message
+        // abort(404);
+
+        return redirect('/');
+    }
+
+    // Set a cache with a key (post.{%slug}) for 30 minutes, if cache value doesn't exist, run the function
+    // Old function (Now replaced with arrow function)
+    // function() use ($path) {
+    //     var_dump('file_get_contents');
+    //     return file_get_contents($path);
+    // }
+    $post = cache()->remember("post.{$slug}", now()->addMinutes(20), fn()=> file_get_contents($path));
+
+    // Non cache version
+    // $post = file_get_contents($path);
+    
+    return view('post', ['post' => $post]);
+// slug validation (failure results in 404)
+// For just alphanumeric characters
+// ->whereAlpha('slug')
+// The above can be replaced with built in
+})->where('slug', '[A-z_\-]+');
