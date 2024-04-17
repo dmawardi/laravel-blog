a68cd2b15f86d4ab3472719f440ecc8a93df1eff
dmawardi <dmawardi@gmail.com>
2024-04-17 13:50:13 +0800

    Created comments migration

--- database/migrations/2024_04_16_132334_create_comments_table.php
+++ database/migrations/2024_04_16_132334_create_comments_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->text('body');
         });
     }
 
